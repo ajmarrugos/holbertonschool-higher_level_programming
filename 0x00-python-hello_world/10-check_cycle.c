2d1d3352ccd3784135f9ff46e63c9b3ed653d0b6
ajmarrugos <ajmarrugos@gmail.com>
2022-05-03 00:00:39 -0500

    Maybe IF we check with C

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,19 @@
+#include lists.h
+
+/**
+* check_cycle - Will look for cilces in linked lists
+* @list: Pointer to a list
+* Return: 0 if no clycle is found, otherwise return 1
+**/
+
+int check_cycle(listint_t *list)
+{
+	if (cicle)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
