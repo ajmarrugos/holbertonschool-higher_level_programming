eadc5ae88df737d7437527f423409a06e182c4ae
ajmarrugos <ajmarrugos@gmail.com>
2022-05-03 19:18:03 -0500

    Returned to original variable number

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 import random
-num = random.randint(-10, 10)
-
-if num == 0:
-	print('{0} is zero'.format(num))
-elif num < 0:
-	print('{0} is negative'.format(num))
-elif num > 0:
-	print('{0} is positive'.format(num))
+number = random.randint(-10, 10)
+if number == 0:
+	print('{0} is zero'.format(number))
+elif number < 0:
+	print('{0} is negative'.format(number))
+elif number > 0:
+	print('{0} is positive'.format(number))
