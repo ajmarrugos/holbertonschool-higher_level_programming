3f3e83f23ac7c113314ac147c7ac25ca48f63884
ajmarrugos <ajmarrugos@gmail.com>
2022-05-03 19:07:36 -0500

    Fixed spaces with tabs + new line ending in code

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -3,8 +3,9 @@ import random
 num = random.randint(-10, 10)
 
 if (num > 0):
-    print(f"{num} is positive")
+	print(f"{num} is positive")
 elif (num == 0):
-    print(f"{num} is zero")
+	print(f"{num} is zero")
 else:
-    print(f"{num} is negative")
+	print(f"{num} is negative")
+	
