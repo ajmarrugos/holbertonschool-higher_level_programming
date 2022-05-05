f4a983c3597acf54bf1974c9a28f79d9d4c418a3
ajmarrugos <ajmarrugos@gmail.com>
2022-05-05 12:36:05 -0500

    Function to make simple maths

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    print((a), '+', (b), '= ', f'{add(a, b)}')
+    print((a), '-', (b), '= ', f'{sub(a, b)}')
+    print((a), '*', (b), '= ', f'{mul(a, b)}')
+    print((a), '/', (b), '= ', f'{div(a, b)}')
