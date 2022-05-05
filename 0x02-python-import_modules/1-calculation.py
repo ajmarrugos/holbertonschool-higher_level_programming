91b6e7640d1baba5d49ecdff6abf114b8e3f32b4
ajmarrugos <ajmarrugos@gmail.com>
2022-05-05 12:41:27 -0500

    Changed print format method

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,7 +3,7 @@ if __name__ == '__main__':
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    print((a), '+', (b), '= ', f'{add(a, b)}')
-    print((a), '-', (b), '= ', f'{sub(a, b)}')
-    print((a), '*', (b), '= ', f'{mul(a, b)}')
-    print((a), '/', (b), '= ', f'{div(a, b)}')
+    print(f"{a} + {b} = {add(a, b)}")
+    print(f"{a} - {b} = {sub(a, b)}")
+    print(f"{a} * {b} = {mul(a, b)}")
+    print(f"{a} / {b} = {div(a, b)}")
