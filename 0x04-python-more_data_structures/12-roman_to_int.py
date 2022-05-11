7ba3dae15e8c945ce4de34a41c45979b940623e1
ajmarrugos <ajmarrugos@gmail.com>
2022-05-10 23:02:15 -0500

    Fixed with PEP8

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,12 +2,22 @@
 def roman_to_int(roman_string):
     if not roman_string or type(roman_string) != str:
         return 0
-    roman_dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    r_dic = {
+        "I": 1,
+        "IV": 4,
+        "V": 5,
+        "IX": 9,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
     roman_num = 0
     for j in range(len(roman_string)):
-        if j > 0 and roman_dic[roman_string[j]] > roman_dic[roman_string[j - 1]]:
-            roman_num += roman_dic[roman_string[j]] - 2 * \
-                        roman_dic[roman_string[j - 1]]
+        if j > 0 and r_dic[roman_string[j]] > r_dic[roman_string[j - 1]]:
+            roman_num += r_dic[roman_string[j]] - 2 * \
+                        r_dic[roman_string[j - 1]]
         else:
-            roman_num += roman_dic[roman_string[j]]
+            roman_num += r_dic[roman_string[j]]
     return roman_num
