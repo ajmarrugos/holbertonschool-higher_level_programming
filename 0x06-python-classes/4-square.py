38579d86776fa13ad54532f46036a8f38c6ad8ed
ajmarrugos <ajmarrugos@gmail.com>
2022-05-24 17:47:50 -0500

    Square class object + exceptions + attribute + method to get and set new attribute value

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Square class with definition
+Includes a 'size' instance/attribute
+Includes TypeError & ValueError exceptions
+Includes an 'area' method functon
+Includes an 'size.setter' method function
+"""
+
+
+class Square:
+    """Represents an empty square"""
+    def __init__(self, size=0):
+        """
+        Init class with args: size
+        runs a check to ensure size data is correct type and value.
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Method function to return the 'area' of the Square"""
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        """Method to return the value stored in the private variable 'size'"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Method which sets a new value to the 'size' instace/attribute"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
