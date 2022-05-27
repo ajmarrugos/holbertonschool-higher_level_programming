398a8d7f09477530bf73e1b24183d5c64cedcac3
ajmarrugos <ajmarrugos@gmail.com>
2022-05-27 15:18:52 -0500

    Function now casts the parameters from float to int

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,6 +3,8 @@
 add_integer:
     - Checks if parameters are bool
     - Checks if parameters are int or float
+    - Casts the parameters from float to int
+    - Returns sum of parameters
 """
 
 
@@ -20,5 +22,10 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
+
+    if type(a) == float:
+        a = int(a)
+    if type(b) == float:
+        b = int(b)
 
     return (a + b)
