52babd8383a9c9357fce41769e0cc4de10b8cc6a
ajmarrugos <ajmarrugos@gmail.com>
2022-05-31 15:30:18 -0500

    Class definition tested and working

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Module: 0-rectangle.py
+Module: 1-rectangle.py
 """
 
 
@@ -19,22 +19,27 @@ class Rectangle:
 
     @property
     def height(self):
-        """Retrieves the height of a Rectangle instance."""
+        """Retrieves the height of a Rectangle instance """
         return self.__height
 
     @width.setter
     def width(self, value):
         """ Retrieves the width of a Rectangle instance """
-        if type(width) is not int:
+        if type(value) is not int:
             raise TypeError("width must be an integer")
 
-        if width < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
+
+        self.__width = value
 
     @height.setter
     def height(self, value):
-        if type(height) is not int:
+        """ Retrieves the height of a Rectangle instance """
+        if type(value) is not int:
             raise TypeError("height must be an integer")
 
-        if height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
+
+        self.__height = value
