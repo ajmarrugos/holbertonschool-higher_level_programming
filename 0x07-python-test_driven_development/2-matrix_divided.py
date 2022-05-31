4d078b18ba5c28cc328cb26f3bfad8548f5d5d8c
ajmarrugos <ajmarrugos@gmail.com>
2022-05-31 00:37:06 -0500

    Better variable specifications

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python3
 """
 matrix_divided:
+    - Declare and define variable strings to short the syntax
     - Divides each element of a matrix of numbers by a number
     - Checks if the entire list is int/float
 """ 
-"""Defined errors"""
-    err_type = "matrix must be a matrix (list of lists) of integers/floats"
+
+    not_matrix = "matrix must be a matrix (list of lists) of integers/floats"
     err_size = "Each row of the matrix must have the same size"
 
 
 def matrix_divided(matrix, div):
 
     if not isinstance(matrix, list):
-        raise TypeError(err_type)
+        raise TypeError(not_matrix)
     if len(matrix) == 0:
-        raise TypeError(err_type)
+        raise TypeError(not_matrix)
 
     new_matrix = []
     
     if isinstance(matrix[0], bool):
-        raise TypeError(err_type)
+        raise TypeError(not_matrix)
     try:
         len_fst_row = len(matrix[0])
     except Exception:
-        raise TypeError(err_type)
+        raise TypeError(not_matrix)
 
     for list_x in matrix:
     
