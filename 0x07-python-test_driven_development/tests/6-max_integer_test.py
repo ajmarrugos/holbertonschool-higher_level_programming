3c752d1e9250adff8433c1fc097c18ff3cd44871
ajmarrugos <ajmarrugos@gmail.com>
2022-06-02 17:16:36 -0500

    fixed erased import

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -3,6 +3,9 @@
 Unittest for: max_integer([..])
 """
 
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
 class TestMaxInteger(unittest.TestCase):
     '''
     unittest class for testing max_integer function.
