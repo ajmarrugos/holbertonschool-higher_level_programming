fd075b121c6a9f2e3ffe587a18a9109c429ddd5e
ajmarrugos <ajmarrugos@gmail.com>
2022-06-02 16:38:43 -0500

    Updated function with test cases

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
+"""
+Module: 4-print_square.py
+Doctest: 4-print_square.txt
+"""
 
-"""Mudule Divide"""
+
 def print_square(size):
+    """ Function that prints a square """
+
+    if not type(size) is (int) or type(size) == float:
+        raise TypeError('size must be an integer')
+    if size < 0:
+        raise ValueError('size must be >= 0')
+
+    for i in range(0, size):
+        for z in range(0, size):
+            print('#', end='')
+        print()
