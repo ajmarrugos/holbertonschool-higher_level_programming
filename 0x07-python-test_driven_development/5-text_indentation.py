b051d058aee81ac75d0b174a9d564f519f0dd901
ajmarrugos <ajmarrugos@gmail.com>
2022-06-02 16:43:44 -0500

    Text identation function with basic tests

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,4 +1,25 @@
 #!/usr/bin/python3
+"""
+Module: 5-text_indentation.py
+Doctest: 5-text_indentation.txt
+"""
 
-"""Mudule Divide"""
+
 def text_indentation(text):
+    """ Function that prints a text with 2 new lines """
+    flag = True
+
+    if type(text) != (str):
+        raise TypeError("text must be a string")
+
+    for i in text:
+        if i == ' ' and flag is True:
+            continue
+        if i in ['.', '?', ':']:
+            print(i, end="")
+            print()
+            print()
+            flag = True
+        else:
+            print(i, end="")
+            flag = False
