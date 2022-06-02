b8f73443c9dd8e99f9dd35b808441685fd5e7442
ajmarrugos <ajmarrugos@gmail.com>
2022-06-02 17:12:50 -0500

    Last test cases

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -20,34 +20,49 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_positive_int_start(self):
         ''' test result of positive int in list at start '''
-        test = [60, 2, 23, 54, 15]
+        test = [50, 2, 23, 74, 25]
         self.assertEqual(max_integer(test), 60)
 
     def test_positive_int_middle(self):
         ''' test result of positive int in list in mid '''
-        test = [3, 2, 6, 4, 5]
+        test = [5, 4, 6, 2, 3]
         self.assertEqual(max_integer(test), 6)
 
     def test_positive_int_end(self):
         '''test result of positive int in list at end'''
-        test = [16, 22, 35, 41, 60]
+        test = [16, 52, 35, 11, 90]
         self.assertEqual(max_integer(test), 60)
 
     def test_mixed_int(self):
-        '''test result of mixed int in list'''
-        test = [16, 22, -35, 41, -60]
+        ''' test result of mixed int in list '''
+        test = [16, 52, -35, 11, -90]
         self.assertEqual(max_integer(test), 41)
 
     def test_negative_ints(self):
-        '''test result of negative int in list'''
+        ''' test result of negative int in list '''
         test = [-16, -1, -35, -41, -60]
         self.assertEqual(max_integer(test), -1)
 
     def test_tuple_pass(self):
-        '''test result of pass tuple'''
+        ''' test result of pass tuple'''
         test = (41, 60)
         self.assertEqual(max_integer(test), 60)
 
+    def test_empty_list(self):
+        """ tests result of empty list [] """
+        test = []
+        self.assertIsNone(max_integer(test))
+
+    def test_None_pass(self):
+        ''' test resutlf of passing Non e'''
+        with self.assertRaises(TypeError):
+            max_integer(None)
+
+    def test_string_pass(self):
+        '''test result of pass string'''
+        test = [-26, -1, "Alberto", -21, -50]
+        with self.assertRaises(TypeError):
+            max_integer(test)
 
 if __name__ == "__main__":
     unittest.main()
