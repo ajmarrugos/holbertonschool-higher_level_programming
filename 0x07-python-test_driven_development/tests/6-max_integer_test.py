7e7f2d5db85021127a8cfda286ec839ae238b1bd
ajmarrugos <ajmarrugos@gmail.com>
2022-06-02 16:56:37 -0500

    Added new unittests to file

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -27,3 +27,23 @@ class TestMaxInteger(unittest.TestCase):
         ''' test result of positive int in list in mid '''
         test = [3, 2, 6, 4, 5]
         self.assertEqual(max_integer(test), 6)
+
+    def test_positive_int_end(self):
+        '''test result of positive int in list at end'''
+        test = [16, 22, 35, 41, 60]
+        self.assertEqual(max_integer(test), 60)
+
+    def test_mixed_int(self):
+        '''test result of mixed int in list'''
+        test = [16, 22, -35, 41, -60]
+        self.assertEqual(max_integer(test), 41)
+
+    def test_negative_ints(self):
+        '''test result of negative int in list'''
+        test = [-16, -1, -35, -41, -60]
+        self.assertEqual(max_integer(test), -1)
+
+    def test_tuple_pass(self):
+        '''test result of pass tuple'''
+        test = (41, 60)
+        self.assertEqual(max_integer(test), 60)
