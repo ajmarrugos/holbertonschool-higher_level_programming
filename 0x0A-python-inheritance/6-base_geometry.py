cf86e7fd3f17dcef05e0f463d8fe591f296fca8f
ajmarrugos <ajmarrugos@gmail.com>
2022-06-07 22:29:19 -0500

    Defines a class with a public instance and exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module: 6-base_geometry.py
+"""
+
+
+class BaseGeometry:
+    """
+    Class for BaseGeometry
+    """
+    def area(self):
+        """
+        Causes exception when called
+        """
+        raise Exception("area() is not implemented")
