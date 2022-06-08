20f4171b7a6b505ac1b26b6687bd450e3ddb16c8
ajmarrugos <ajmarrugos@gmail.com>
2022-06-07 22:07:16 -0500

    check class to inherit from a class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module: 3-is_kind_of_class.py
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """- Finds if obj is an instance of a_class or inherited from a_class """
+
+    return isinstance(obj, a_class)
---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module: 4-inherits_from.py
+"""
+
+
+def inherits_from(obj, a_class):
+    """ Function that returns True if the object is an instance
+    of a class that inherited (directly or indirectly) from the
+    specified class; otherwise False.
+    """
+
+    if type(obj) is a_class or not isinstance(obj, a_class):
+        return False
+    else:
+        return True
