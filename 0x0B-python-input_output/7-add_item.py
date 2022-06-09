b6570e1a316d68b8868df16539f8e2371567a479
ajmarrugos <ajmarrugos@gmail.com>
2022-06-08 20:57:29 -0500

    Added functon prototype

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,19 +8,21 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-"""
-This mdoule will Add all arguments to a Python list,
-and then save them to a file.
-"""
 
-try:
-    json_list = load_from_json_file('add_item.json')
-except FileNotFoundError:
-    json_list = []
+def add_item(filename="add_item.json"):
+    """
+    Will Add all arguments to a Python list,
+    and then save them to a file.
+    """
 
-for i in range(len(sys.argv)):
-    if i == 0:
-        continue
-    json_list.append(sys.argv[i])
+    try:
+        json_list = load_from_json_file('add_item.json')
+    except FileNotFoundError:
+        json_list = []
 
-save_to_json_file(json_list, 'add_item.json')
+    for i in range(len(sys.argv)):
+        if i == 0:
+            continue
+        json_list.append(sys.argv[i])
+
+    save_to_json_file(json_list, 'add_item.json')
