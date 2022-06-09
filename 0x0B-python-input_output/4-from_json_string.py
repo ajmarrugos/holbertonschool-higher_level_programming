c3ca890b6d9d893562f88e902f5a283244562c3a
ajmarrugos <ajmarrugos@gmail.com>
2022-06-08 20:52:10 -0500

    Wrong file copy

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -7,8 +7,6 @@ Module: 4-from_json_string.py
 import json
 
 
-def read_file(filename=""):
-    """ Opens and prints file contents to stdout """
-
-    with open(filename, encoding="utf-8") as f:
-        print(f.read(), end="")
+def from_json_string(my_str):
+    """ Returns an Python Object represented by a JSON string """
+    return json.loads(my_str)
