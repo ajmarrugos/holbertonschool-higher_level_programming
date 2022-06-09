5c19da1f7e18e42ca760367f6ad0d19495570a09
ajmarrugos <ajmarrugos@gmail.com>
2022-06-08 20:42:08 -0500

    Return a JSON formatted object

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module: 3-to_json_string.py
+"""
+
+
+import json
+
+def to_json_string(my_obj):
+    """ Return a JSON formatted object """
+
+    return json.dumps(my_obj)
