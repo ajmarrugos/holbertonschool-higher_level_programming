ad3975ac079a76448a376e211a7bc5de04eda816
ajmarrugos <ajmarrugos@gmail.com>
2022-06-08 21:28:09 -0500

    Object to json

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module: 10-student.py
+"""
+
+
+class Student:
+    """ Representation of a student """
+    def __init__(self, first_name, last_name, age):
+        """ Constructor for Student Object """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Will return a dictionary representation of a 
+        Student instance with specified attributes """
+        if attrs is None:
+            return self.__dict__
+        new_dic = {}
+        for a in attrs:
+            try:
+                new_dic[a] = self.__dict__[a]
+            except:
+                pass
+        return new_dic
