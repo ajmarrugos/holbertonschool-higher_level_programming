13689528aff3ecfaa2a05582f92b5b231a6fa0fd
ajmarrugos <ajmarrugos@gmail.com>
2022-06-14 13:42:41 -0500

    Added validation method for setters to verify attribute data

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -11,10 +11,10 @@ class Rectangle(Base):
     """Returns the width attributes"""
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     """ATTRIBUTE GETTERS"""
     @property
@@ -37,23 +37,39 @@ class Rectangle(Base):
         """Returns the y attribute"""
         return self.__y
 
+    """ATTRIBUTE VALIDATION METHOD"""
+    def int_val(self, atts, value):
+        """Validates entry data in attributes"""
+        if type(value) is not int:
+            raise TypeError(f"{atts} must be an integer")
+        if atts == "width" or atts == "height":
+            if value <= 0:
+                raise ValueError(f"{atts} must be > 0")
+        if atts == "x" or atts == "y":
+            if value < 0:
+                raise ValueError(f"{atts} must be >= 0")
+
     """ATTRIBUTE SETTERS"""
     @width.setter
     def width(self, value):
-        """Sets the width attributes."""
+        """Sets the width attribute"""
+        self.int_val("width", value)
         self.__width = value
 
     @height.setter
     def height(self, value):
-        """Sets the height attributes."""
+        """Sets the height attribute"""
+        self.int_val("height", value)
         self.__height = value
 
     @x.setter
     def x(self, value):
-        """Sets the x attributes."""
+        """Sets the x attribute"""
+        self.int_val("x", value)
         self.__x = value
 
     @y.setter
     def y(self, value):
-        """Sets the y attributes."""
+        """Sets the y attribute"""
+        self.int_val("y", value)
         self.__y = value
