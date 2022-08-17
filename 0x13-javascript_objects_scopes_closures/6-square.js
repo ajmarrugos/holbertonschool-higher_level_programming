415a1bf8ad7f16936dcc7a7d725691f46bbcf0f7
ajmarrugos <ajmarrugos@gmail.com>
2022-08-16 23:56:25 -0500

    Squares

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,19 +1,18 @@
 #!/usr/bin/node
-const Square1 = require('./4-rectangle');
+const Rectangle = require('./4-rectangle');
 
-class Square extends Rectangle {
-    constructor (size) {
-      super(size, size);
-    }
-  
-    charPrint (c) {
-      if (c) {
-        for (let i = 0; i < this.height; i++) {
-          console.log(c.repeat(this.width));
-        }
-      } else {
-        this.print();
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c) {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
       }
+    } else {
+      this.print();
     }
   }
-  module.exports = Square;
+};
