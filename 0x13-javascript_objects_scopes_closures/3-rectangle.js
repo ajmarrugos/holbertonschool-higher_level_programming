70151aa7ba0a4fc951dc2a246ea535d9bb508179
ajmarrugos <ajmarrugos@gmail.com>
2022-08-16 23:34:36 -0500

    First Rectangles

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+class Rectangle {
+    constructor (w, h) {
+      if (w > 0 && h > 0) {
+        this.width = w;
+        this.height = h;
+      }
+    }
+  
+    print () {
+      for (let r = 0; r < this.height; r++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+  
+  module.exports = Rectangle;
