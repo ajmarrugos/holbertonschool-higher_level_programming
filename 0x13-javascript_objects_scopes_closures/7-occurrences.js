3aeb8f26278bf7bb6c3b87ddd45b30a2e7194c6c
ajmarrugos <ajmarrugos@gmail.com>
2022-08-17 00:03:38 -0500

    Ocurrence?

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 exports.nbOccurences = function (list, searchElement) {
-  let counter = 0;  
+  let counter = 0;
   for (const i in list) {
     if (list[i] === searchElement) {
       counter++;
