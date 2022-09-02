5142a463fa906d71366a3a9d32b5e38b6c26a74d
root <root@Holberton.localdomain>
2022-09-02 17:01:57 -0500

    JSON POST request to a URL: Body

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a JSON POST request to a URL passed as the first argument,and displays the body of the response.
-curl -sX POST "$1" -H 'Content-Type: application/json' -d "$(cat "$2")"
+curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
