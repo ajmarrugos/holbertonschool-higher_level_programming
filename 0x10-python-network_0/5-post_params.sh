d1f091e27e6a28d5cfb8aedc40d83c99bf480fa6
root <root@Holberton.localdomain>
2022-09-02 16:30:18 -0500

    POST request URL: Body

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -sX "$1"  POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+curl -sX POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
